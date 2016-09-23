a8b2a037808d7bd21d48821bda81627bdb231be8
Adolfo Raya <araya0912@hotmail.com>
2016-09-23 00:32:58 -0500

    Hola mundo en IDE Eclipse

---
+++ java/proyetos/HolaMundoIDE/src/com/araya0912/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * HolaMundo.java
+ * Muestra en pantalla el mensjae "Hola Mundo :D"
+ * 
+ * Autor: Alejandro Adolfo Perez Raya
+ * Última Modificación: 23 de septiembre de 2016
+ * */
+package com.araya0912.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo :D");
+	}
+
+}
