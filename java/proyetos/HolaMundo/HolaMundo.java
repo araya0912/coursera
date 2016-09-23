298a26f18d72ab6bff647afa89ed1852366cf6b3
Adolfo Raya <araya0912@hotmail.com>
2016-09-23 00:13:35 -0500

    Hola mundo en editor de texto

---
+++ java/proyetos/HolaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+/*
+  HolaMundo.java
+  Despliega en consola el mensaje "Hola Mundo :D"
+
+  Autor: Alejandro Adolfo Perez Raya
+  Última modificación: 22 de septiembre de 2016
+*/
+public class HolaMundo {
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo :D");
+  }
+}
